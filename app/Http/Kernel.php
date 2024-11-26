9e490fa187c35e1df4559b7a3e5691e9fb18b1ae
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2024-11-26 23:01:14 +0700

    `feat`: configure JWT

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.jwt' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+        'role' => \App\Http\Middleware\RoleMiddleware::class,
     ];
 }
