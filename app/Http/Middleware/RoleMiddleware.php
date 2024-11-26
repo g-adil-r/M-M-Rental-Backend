ff8ba2df14e0d0d55ac215aff294f18f286c79c9
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2024-11-26 23:03:09 +0700

    `feat`: add role middleware for future use

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if(auth()->user()->role->name !== $role) {
+            return response()->json([
+                "status" => [
+                    "code" => 401,
+                    "is_success" => false,
+                ],
+                "message" => "Unauthorized",
+                "data" => null,
+            ]);
+        }
+
+        return $next($request);
+    }
+}
